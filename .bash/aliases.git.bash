2930d191b1c080e92e7150c7db46d84f34726aba
Alexander Tarmolov <hevil@yandex-team.ru>
2013-07-29 14:32:33 +0400

    use cmpf if it exists

--- .bash/aliases.git.bash
+++ .bash/aliases.git.bash
@@ -2,9 +2,7 @@
 
 alias gs="git st"
 alias gd="git di"
-cmpf _git_diff gd
 alias gdc="git dic"
-cmpf _git_diff gdc
 alias ga="git add"
 alias gsh="git show"
 alias gc="git commit -m"
@@ -12,21 +10,26 @@ alias gca="git commit -a -m"
 alias gf="git app"
 alias gfa="git app -a"
 alias gp="git pull"
-cmpf _git_pull gp
 alias gb="git branch"
-cmpf _git_branch gb
 alias gbd="git branch -D"
-cmpf _git_branch gbd
 alias gba="git branch -a"
 alias gcob="git checkout -b"
-cmpf _git_checkout gcob
 alias gco="git checkout"
-cmpf _git_checkout gco
 alias gcp="git cherry-pick"
-cmpf _git_cherry-pick gcp
 alias glg="git lg"
-cmpf _git_log glg
 alias gh="git hist"
-cmpf _git_log gh
 alias gr='cd "`git rev-parse --show-toplevel`"'
 alias gv="git vommit"
+
+if hash cmpf 2>/dev/null; then
+    cmpf _git_diff gd
+    cmpf _git_diff gdc
+    cmpf _git_pull gp
+    cmpf _git_branch gb
+    cmpf _git_branch gbd
+    cmpf _git_checkout gcob
+    cmpf _git_checkout gco
+    cmpf _git_cherry-pick gcp
+    cmpf _git_log glg
+    cmpf _git_log gh
+fi
