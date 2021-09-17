479be7c5465f3ed0878192b97a1d9a3962dd530b
Alexander Tarmolov <hevil@yandex-team.ru>
2021-09-17 19:15:43 +0300

    Fix arc prompt

--- .bash/arc-prompt.bash
+++ .bash/arc-prompt.bash
@@ -3,8 +3,8 @@
 __arc_ps1 () {
     local exit=$?
 
-    if [[ $PWD =~ "/arc/" ]]; then
-        printf -- " (%s)" `cat $HOME/arc/store/.arc/HEAD | awk -F '"' '{print $2}'`
+    if [[ $PWD =~ "/arcadia/" ]]; then
+        printf -- " (%s)" `cat $HOME/.store/.arc/HEAD | awk -F '"' '{print $2}'`
     fi
     return $exit
 }
