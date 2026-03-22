0f306c89acd693b9d367101d3121210ecaf10b60
cogery-agent <hevil.crc@gmail.com>
2026-03-22 04:46:47 +0300

    fix: replace deprecated :BundleInstall with :PluginInstall in build.sh

--- build.sh
+++ build.sh
@@ -64,7 +64,7 @@ echo "Install vim plugins..."
 cd ~/.config/tarmolov
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sleep 1
-vim -c ":BundleInstall" -c ":qa"
+vim -c ":PluginInstall" -c ":qa"
 cd - >> /dev/null
 
 echo "Generate .profile and .gitconfig"
